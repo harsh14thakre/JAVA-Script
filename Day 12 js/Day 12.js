3721c5cb3fade7b2cef4c81f93d90f77d56c1a75
harsh14thakre <harsh14thakre@gmail.com>
2025-02-25 16:24:43 +0530

    update

---
+++ Day 12 js/Day 12.js
@@ -0,0 +1,12 @@
+//-------------------------- Events are actions performed by the user---------------------------------
+//1. Mouse Event -  
+// (Onclick)
+// (Doubleclick)
+// (Onmouseenter)
+
+function fun()
+{
+    let a=document.getElementById('head')
+    a.textContent="Zero"
+    
+}
