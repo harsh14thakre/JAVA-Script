c3566911f98d2624076833e784936d7306b7ed60
harsh14thakre <harsh14thakre@gmail.com>
2025-02-28 23:03:35 +0530

    updated

---
+++ Day 14 js/Day 14 queryselector.js
@@ -0,0 +1,83 @@
+
+
+// let b=document.querySelector(".head")
+// b.innerHTML="hello"
+// b.style.backgroundColor="red"
+
+
+// let h1=document.getElementsByClassName('head')
+// h1[0].style.color="white"
+
+// let headid=document.querySelector("#hid")
+// headid.style.color="blue"
+
+
+// let  list = document.querySelectorAll('li')
+// list[2].style.backgroundColor="purple"
+
+
+
+
+
+
+
+// -----------------------------------------Q1---------------------------------------------
+
+
+let a="harsh thakre"
+console.log(a)
+
+
+// --------------------------------------------Q2---------------------------------------------
+
+let b=12
+let c=13
+let empty=b
+b=c
+c=empty
+document.write("b="+b+"<br>"+"c="+c)
+
+// --------------------------------------------O3------------------------------------------
+
+
+// let num=parseInt(prompt("Enter any number"))
+// if(num%2==0)
+// {
+//     document.write("Number is even")
+// }
+// else{
+//     document.write("Number is odd")
+// }
+
+
+// --------------------------------Q4------------------------------------------------------
+
+let age=parseInt(prompt("Enter your age"))
+if(age>=18)
+{
+    document.write("you are eligible")
+}
+else{
+    document.write("you are not eligible")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
