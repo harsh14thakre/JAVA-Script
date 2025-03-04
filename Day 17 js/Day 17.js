c8c4c24b863cf4cd3be2ee55659faf49bf6fdfd9
harsh14thakre <harsh14thakre@gmail.com>
2025-03-04 16:32:21 +0530

    commit

---
+++ Day 17 js/Day 17.js
@@ -0,0 +1,20 @@
+let count=0
+
+function st(){
+
+   let h=document.querySelector("#num")
+}
+let sr=setInterval(()=>{h.innerHTML=count++},500)
+
+function stop()
+{
+    clearInterval(sr)
+}
+
+
+
+
+
+
+
+
