6095f654cd2000d9909f0cbaa8174aaee063ace2
harsh14thakre <harsh14thakre@gmail.com>
2025-03-11 15:11:59 +0530

    updated

---
+++ Day 21 js/Day 21.js
@@ -0,0 +1,50 @@
+// let btn=document.querySelector('#btn')
+// let ome=document.querySelector('#ome')
+// let odbc=document.querySelector('#odbc')
+// let oml=document.querySelector('#oml')
+
+
+//   btn.addEventListener('click',()=>{alert("Hello")})
+//   ome.addEventListener('mouseenter',()=>{alert("Hello")})
+//   odbc.addEventListener('dblclick',()=>{alert("Hello")})
+//   oml.addEventListener('mouseleave',()=>{alert("Hello")})
+
+
+
+
+//   // Array -> Collection of different type of data
+//   // [] , ordered
+//   // index => Start from 0
+
+//   let arr = [12,34.5,'Cybrom',true]
+//   console.log(arr[2])   //to print single element
+
+//   let arr1 = [12,34.5,'Cybrom',true]
+//   console.log(arr1)     // to print full Array
+
+//   let arr2 = [12,34.5,'Cybrom',true]
+
+//   for (let vl of arr1)
+//   {
+//     console.log(vl)
+//   }
+
+//   //for index value
+
+//   let arr3 = [12,34.5,'Cybrom',true]
+//   for(let k in arr3)
+//   {
+//     console.log(k)
+//   }
+
+
+
+
+  let arr4=[]
+  for(let i=0; i<5; ++i)
+  {
+    arr4.parseInt(prompt("Enter the value"))
+  }
+  console.log(arr4)
+   
+  
