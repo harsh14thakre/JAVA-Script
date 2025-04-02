4c4bf0e080eb0233fbbb85b7e130a4fd96ed3981
harsh14thakre <harsh14thakre@gmail.com>
2025-04-02 15:36:26 +0530

    updated

---
+++ Day 27 local storage/login.js
@@ -0,0 +1,87 @@
+// <!-- local storage
+
+// set item - save karana hai
+// get item -  save hai retrieve karana hai
+// remove item - particular data ko delete kaarna hai
+// clear  local - function me jitna bhi data hai sabko clear karne ke kaam me ata hai -->
+
+
+// local storage me data set key value ke formate me hoga 
+// key value ka type string hoga
+
+
+let signf = document.querySelector('#signf')
+let logf = document.querySelector('#logf')
+
+function sign(){
+
+let uname=document.querySelector('#uname').value
+let upass=document.querySelector('#upass').value
+    
+     if(upass==""){
+        alert("please Enetr the password")
+        document.querySelector('#upass').focus()
+        return false
+      }
+      else if(!(upass.match(/[@_#]/))){
+        alert("please use speacial character")
+        document.querySelector('#pass').focus()
+        return false
+      }
+      localStorage.setItem("Username",uname)
+      localStorage.setItem("Userpass",upass)
+
+location.href="./login.html"
+      return false
+}
+
+
+
+function log(){
+  let loginname=document.querySelector('#loginname')
+
+}
+
+
+
+
+//                      key      value
+// localStorage.setItem("Username","Harsh")
+// localStorage.setItem("Userage","21")
+// localStorage.setItem("Usercontact","123456789")
+
+// let name=localStorage.getItem("Username")
+// console.log(name)
+// let age=localStorage.getItem("Userage")
+// console.log(age)
+
+// localStorage.removeItem('Userage')  // removes perticular item
+// localStorage.clear()    //removes all data
+
+
+
+
+// let obj={
+//     name:"Harsh", contact:123456789, city:"Bhopal"
+// }
+// localStorage.setItem("userdata",JSON.stringify(obj))
+
+// let user=JSON.parse(localStorage.getItem('userdata'))
+// console.log(user.city)
+
+// function getdataa()
+// {
+//      let name=document.querySelector('#name')
+//      let age=document.querySelector('#age')
+//      let gmail=document.querySelector('#gmail')
+// }
+
+// let obj1={
+    
+// }
+
+
+
+
+
+
---
+++ Day 28 template literals js/api.js
@@ -0,0 +1,32 @@
+// String=> Collection of characters
+
+// let name="Harsh"
+
+// let name1=` My name is ${name} asdfghjkll`
+
+// console.log(name1)
+
+
+// //functions
+
+// let name2="harsh"
+// console.log(name2.toUpperCase())
+
+// let name3="ADDRESS"
+// console.log(name3.toLowerCase())
+
+// let a=name2.repeat(100)
+// console.log(a)               //
+
+
+// fetch(url,{})
+// => get ,post ,delete, put => http request?method
+
+// =================================================================================
+
+
+// Synchronous=> it executes code line by line 
+// Asynchronous=> it executes code by comparing which line of code needs less time to execute
+// it 
+
+
