54c9f81dd607339df47ccc3b996c280e6c694921
harsh14thakre <harsh14thakre@gmail.com>
2025-04-03 16:32:10 +0530

    updated

--- Day 30 fetch js/script.js
+++ Day 30 fetch js/script.js
@@ -7,6 +7,26 @@ let fetchData=async()=>{
     let data= await res.json()
 
     console.log(data);
+
+    let Show=document.querySelector("#dataShow")
+
+    data.map((e)=>{
+        Show.innerHTML+=`
+        
+           <tr>
+           <td>${e.name}</td>
+           <td>${e.age}</td>
+           <td>${e.adharNO}</td>
+           <td>${e.checkin}</td>
+           <td>${e.checkout}</td>
+           <td>${e.mobileno}</td>
+           <td>${e.person}</td>
+           <td>${e.price}</td>
+           </tr>
+        
+        
+        `
+    })
 }
 
 fetchData()
